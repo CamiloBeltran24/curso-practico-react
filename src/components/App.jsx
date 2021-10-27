e9baaa83faa3d6b6ffed5ec6e3e53a74a61fe412
Camilo Beltran <cambelf.dev@gmail.com>
2021-10-26 19:51:54 -0500

    :recycle: ADD: fin configuracion base, CSS - SCSS
    se ha agregado la configuracion para poder usar css y sass dentro de webpack, con eso se finaliza la seccion de configuracion basica

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import "../styles/global.scss";
 
 const App = () => {
-  return <h1>Hola mundo!</h1>;
+  return <h1>Hola mundo, Bienvenido a mi primera pagina con react</h1>;
 };
 export default App;
